0b992a5c9ac013f9efac51f195fe14fa36616e2d
Steven Blom <atomicblom@binaryvibrance.net>
2017-11-16 23:20:46 +1030

    Room generation is working for the mostpart

--- src/main/java/com/github/atomicblom/eightyone/PlayerEvents.java
+++ src/main/java/com/github/atomicblom/eightyone/PlayerEvents.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod.EventBusSubscriber(Side.SERVER)
+@Mod.EventBusSubscriber()
 public class PlayerEvents
 {
 	@SubscribeEvent
--- src/main/java/com/github/atomicblom/eightyone/world/ChunkGeneratorEightyOne.java
+++ src/main/java/com/github/atomicblom/eightyone/world/ChunkGeneratorEightyOne.java
@@ -6,16 +6,28 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import net.minecraft.block.BlockFalling;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.IResource;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Blocks;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.datafix.FixTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.NoiseGeneratorSimplex;
+import net.minecraft.world.gen.structure.template.PlacementSettings;
+import net.minecraft.world.gen.structure.template.Template;
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -23,6 +35,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ChunkGeneratorEightyOne implements IChunkGenerator
 {
+	private static final int ROOM_SET_SIZE = 5;
+	private static final int BASE_HEIGHT = 8;
 	private final World world;
 	private final long seed;
 	private final boolean mapFeaturesEnabled;
@@ -76,6 +90,7 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 
 	private void generateChunk(int chunkX, int chunkZ, ChunkPrimer primer)
 	{
+		final boolean renderRoof = false;
 		final int height = 6;
 		final int posX = chunkX << 4;
 		final int posZ = chunkZ << 4;
@@ -88,22 +103,26 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 				if (!room.hasProperty(RoomProperties.IsPresent)) continue;
 				if (room.contains(posX + x, posZ + z))
 				{
-					primer.setBlockState(x, 64, z, Blocks.COBBLESTONE.getDefaultState());
+					primer.setBlockState(x, BASE_HEIGHT, z, Blocks.COBBLESTONE.getDefaultState());
 
 					final int xOffset = room.getXOffset(posX + x);
 					final int zOffset = room.getZOffset(posZ + z);
 
-					if ((xOffset == 2 || xOffset == 3 || xOffset == 5 || xOffset == 6) &&
-						(zOffset == 2 || zOffset == 3 || zOffset == 5 || zOffset == 6))
+					if (renderRoof)
 					{
-						primer.setBlockState(x, 64 + height, z, Blocks.GLASS.getDefaultState());
-					} else {
-						primer.setBlockState(x, 64 + height, z, Blocks.COBBLESTONE.getDefaultState());
+						if ((xOffset == 2 || xOffset == 3 || xOffset == 5 || xOffset == 6) &&
+								(zOffset == 2 || zOffset == 3 || zOffset == 5 || zOffset == 6))
+						{
+							primer.setBlockState(x, BASE_HEIGHT + height, z, Blocks.GLASS.getDefaultState());
+						} else
+						{
+							primer.setBlockState(x, BASE_HEIGHT + height, z, Blocks.COBBLESTONE.getDefaultState());
+						}
 					}
 
 					if (room.isWall(posX + x, posZ + z))
 					{
-						for (int y = 64; y < 64 + height; ++y)
+						for (int y = BASE_HEIGHT; y < BASE_HEIGHT + height; ++y)
 						{
 							primer.setBlockState(x, y, z, Blocks.COBBLESTONE.getDefaultState());
 						}
@@ -116,27 +135,146 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 	@Override
 	public void populate(int chunkX, int chunkZ)
 	{
+
+
 		BlockFalling.fallInstantly = true;
 
 		if (world.getChunkFromChunkCoords(chunkX, chunkZ).isPopulated()) return;
 
-		int x  = (chunkX << 4);// + 8;
-		int z  = (chunkZ << 4);// + 8;
+		final int chunkCornerX  = chunkX << 4;
+		final int chunkCornerZ  = chunkZ << 4;
 
-		final Room room = getRoomAt(x, z);
-		if (!room.hasProperty(RoomProperties.IsPresent)) return;
-		if (room.hasProperty(RoomProperties.VerticalExit)) {
-			//Generate Vertical exit
+		final int nextChunkX = chunkCornerX + 15;
+		final int nextChunkZ = chunkCornerZ + 15;
 
-			world.setBlockState(new BlockPos(room.getX() + 4, 72, room.getZ()), Blocks.GOLD_BLOCK.getDefaultState());
+		final IBlockState dirt = Blocks.DIRT.getDefaultState();
+		final IBlockState air = Blocks.AIR.getDefaultState();
 
-		} else if (room.hasProperty(RoomProperties.HorizontalExit)) {
-			//Generate
+		final Room cornerRoom = getRoomAt(chunkCornerX - 10, chunkCornerZ - 10);
+		for (int roomX = cornerRoom.getX(); roomX < nextChunkX + 10; roomX += 10) {
+			for (int roomZ = cornerRoom.getZ(); roomZ < nextChunkZ + 10; roomZ += 10) {
 
-			world.setBlockState(new BlockPos(room.getX(), 72, room.getZ() + 4), Blocks.DIAMOND_BLOCK.getDefaultState());
+				final Room room = getRoomAt(roomX, roomZ);
+
+				if (!room.hasProperty(RoomProperties.IsPresent)) continue;
+
+				if (room.hasProperty(RoomProperties.VerticalExit)) {
+					//Generate Vertical exit
+
+					final int xPos = room.getX() + 4;
+					final int zPos = room.getZ() + 8;
+					final MutableBlockPos pos = new MutableBlockPos(xPos, BASE_HEIGHT + 7, zPos);
+
+					for (int i = zPos; i < room.getZ() + 8 + 3; ++i) {
+						for (int y = BASE_HEIGHT; y < BASE_HEIGHT + 4; ++y) {
+							pos.setPos(xPos - 2, y, i);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+							pos.setPos(xPos + 2, y, i);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						}
+
+						pos.setPos(xPos - 1, BASE_HEIGHT, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(xPos, BASE_HEIGHT, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(xPos + 1, BASE_HEIGHT, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+
+						pos.setPos(xPos - 1, BASE_HEIGHT + 4, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(xPos, BASE_HEIGHT + 4, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(xPos + 1, BASE_HEIGHT + 4, i);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+
+						for (int y = BASE_HEIGHT + 1; y < BASE_HEIGHT + 4; ++y) {
+							pos.setPos(xPos - 1, y, i);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+							pos.setPos(xPos + 1, y, i);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+							pos.setPos(xPos, y, i);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+						}
+					}
+
+				}
+				if (room.hasProperty(RoomProperties.HorizontalExit)) {
+					//Generate
+
+					final int xPos = room.getX() + 8;
+					final int zPos = room.getZ() + 4;
+					final MutableBlockPos pos = new MutableBlockPos(xPos, BASE_HEIGHT + 7, zPos);
+
+					for (int i = xPos; i < room.getX() + 8 + 3; ++i) {
+						for (int z = BASE_HEIGHT; z < BASE_HEIGHT + 4; ++z) {
+							pos.setPos(i, z, zPos - 2);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+							pos.setPos(i, z, zPos + 2);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						}
+
+
+						pos.setPos(i, BASE_HEIGHT, zPos - 1);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(i, BASE_HEIGHT, zPos);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(i, BASE_HEIGHT, zPos + 1);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+
+						pos.setPos(i, BASE_HEIGHT + 4, zPos - 1);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(i, BASE_HEIGHT + 4, zPos);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+						pos.setPos(i, BASE_HEIGHT + 4, zPos + 1);
+						placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, dirt);
+
+						for (int z = BASE_HEIGHT + 1; z < BASE_HEIGHT + 4; ++z) {
+							pos.setPos(i, z, zPos - 1);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+							pos.setPos(i, z, zPos + 1);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+							pos.setPos(i, z, zPos);
+							placeBlockIfInChunk(chunkCornerX, chunkCornerZ, pos, air);
+						}
+					}
+				}
+
+				try
+				{
+					final IResource eightyone = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("eightyone", "structures/minimaze.nbt"));
+					NBTTagCompound nbttagcompound = CompressedStreamTools.readCompressed(eightyone.getInputStream());
+
+					if (!nbttagcompound.hasKey("DataVersion", 99))
+					{
+						nbttagcompound.setInteger("DataVersion", 500);
+					}
+
+					Template template = new Template();
+					template.read(Minecraft.getMinecraft().getDataFixer().process(FixTypes.STRUCTURE, nbttagcompound));
+					final PlacementSettings placementSettings = new PlacementSettings();
+					placementSettings.setChunk(new ChunkPos(chunkX, chunkZ));
+					template.addBlocksToWorldChunk(world, new BlockPos(room.getX()+1, BASE_HEIGHT + 1, room.getZ()+1), placementSettings);
+
+				} catch (IOException exception) {
+
+				}
+			}
 		}
 
 		BlockFalling.fallInstantly = false;
+	}
+
+	private void placeBlockIfInChunk(int chunkCornerX, int chunkCornerZ, BlockPos pos, IBlockState blockState)
+	{
+		final int nextChunkX = chunkCornerX + 16;
+		if (pos.getX() >= chunkCornerX && pos.getX() < nextChunkX)
+		{
+			final int nextChunkZ = chunkCornerZ + 16;
+			if (pos.getZ() >= chunkCornerZ && pos.getZ() < nextChunkZ)
+			{
+				world.setBlockState(pos, blockState);
+			}
+		}
 	}
 
 	@Override
@@ -175,6 +313,12 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 		List<RoomSet> roomSets = Lists.newArrayList();
 
 		private Point2DRoomCacheLoader() {
+			MakeRooms();
+		}
+
+		private void MakeRooms()
+		{
+			roomSets.clear();
 			RoomSet e;
 
 //			e = new RoomSet();
@@ -186,42 +330,52 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 //			roomSets.add(e);
 
 			e = new RoomSet();
-			e.presentRooms = new boolean[] {
-					true, true, true,
-					true, false, true,
-					true, true, true
+			e.presentRooms = new int[] {
+					1, 1, 1, 0, 1,
+					0, 0, 1, 0, 0,
+					0, 0, 1, 0, 0,
+					0, 0, 1, 1, 1,
+					0, 0, 0, 0, 1
 			};
 			roomSets.add(e);
 
 			e = new RoomSet();
-			e.presentRooms = new boolean[] {
-					true, true, true,
-					false, false, false,
-					true, true, true
+			e.presentRooms = new int[] {
+					1, 1, 0, 0, 1,
+					0, 1, 0, 0, 1,
+					0, 1, 1, 1, 1,
+					0, 0, 1, 0, 0,
+					0, 0, 1, 1, 1,
 			};
 			roomSets.add(e);
 
 			e = new RoomSet();
-			e.presentRooms = new boolean[] {
-					true, true, true,
-					false, true, false,
-					false, true, false
+			e.presentRooms = new int[] {
+					1, 1, 1, 0, 0,
+					0, 1, 0, 0, 0,
+					0, 1, 0, 0, 0,
+					1, 1, 1, 1, 0,
+					1, 0, 0, 0, 0
 			};
 			roomSets.add(e);
 
 			e = new RoomSet();
-			e.presentRooms = new boolean[] {
-					true, true, false,
-					false, true, false,
-					false, true, true
+			e.presentRooms = new int[] {
+					1, 1, 0, 1, 1,
+					0, 1, 0, 1, 0,
+					0, 1, 1, 1, 0,
+					0, 0, 0, 1, 0,
+					0, 0, 0, 1, 1
 			};
 			roomSets.add(e);
 
 			e = new RoomSet();
-			e.presentRooms = new boolean[] {
-					false, true, true,
-					false, true, false,
-					true, true, false
+			e.presentRooms = new int[] {
+					1, 1, 0, 0, 1,
+					0, 1, 0, 0, 1,
+					0, 1, 1, 0, 0,
+					0, 1, 0, 0, 0,
+					1, 1, 0, 0, 0
 			};
 			roomSets.add(e);
 		}
@@ -229,33 +383,50 @@ public class ChunkGeneratorEightyOne implements IChunkGenerator
 		@Override
 		public Room load(Point2D key) throws Exception
 		{
+			//this.MakeRooms();
 			final int x = key.getX() / 10;
-			final int roomSetX = x / 3;
 			final int z = key.getZ() / 10;
-			final int roomSetZ = z / 3;
-			double noise = noiseGen.getValue(roomSetX, roomSetZ);
-			final int roomSetIndex = (int) (Math.abs(noise) * roomSets.size());
-			RoomSet roomSet = roomSets.get(roomSetIndex);
+
+			boolean isPresent = isRoomPresent(x, z);
 			int properties = 0;
-			int roomX = x % 3;
-			if (roomX < 0) roomX += 3;
-			int roomZ = z % 3;
-			if (roomZ < 0) roomZ += 3;
-			if (roomSet.isPresent(roomX, roomZ)) {
+
+			if (isPresent) {
 				properties = 12;
 			}
+
+			if (isRoomPresent(x, z + 1)) {
+				properties |= RoomProperties.VerticalExit.getBitMask();
+			}
+			if (isRoomPresent(x + 1, z)) {
+				properties |= RoomProperties.HorizontalExit.getBitMask();
+			}
+
 			final Room result = new Room(roomId, key.getX(), key.getZ(), 9, 9, properties);
 			roomId++;
 			return result;
 		}
+
+		public boolean isRoomPresent(int x, int z) {
+			final int roomSetX = x / ROOM_SET_SIZE;
+			final int roomSetZ = z / ROOM_SET_SIZE;
+			double noise = noiseGen.getValue(roomSetX, roomSetZ);
+			final int roomSetIndex = (int) (Math.abs(noise) * roomSets.size());
+			RoomSet roomSet = roomSets.get(roomSetIndex);
+
+			int roomX = x % ROOM_SET_SIZE;
+			if (roomX < 0) roomX += ROOM_SET_SIZE;
+			int roomZ = z % ROOM_SET_SIZE;
+			if (roomZ < 0) roomZ += ROOM_SET_SIZE;
+			return roomSet.isPresent(roomX, roomZ);
+		}
 	}
 
 	private class RoomSet {
-		boolean[] presentRooms = new boolean[9];
+		int[] presentRooms = new int[ROOM_SET_SIZE * ROOM_SET_SIZE];
 
 		public boolean isPresent(int x, int z)
 		{
-			return presentRooms[x * 3 + z];
+			return presentRooms[x * ROOM_SET_SIZE + z] != 0;
 		}
 	}
 }
